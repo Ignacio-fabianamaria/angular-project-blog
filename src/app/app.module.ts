d66223ab9dac54a35a78e0facc751845cfbcaef5
Siigmaf <faby.tbe@gmail.com>
2023-09-14 14:10:06 -0300

    feat:Created project component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuBarComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
